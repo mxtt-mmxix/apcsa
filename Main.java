b5812e0a55572d609a056b6b29e50d008ec58e98
Matthew McCall <matthewm3004@icloud.com>
2021-03-02 13:58:46 -0500

    Lesson 61 - Sequential Search ArrayList

--- Main.java
+++ Main.java
@@ -1,109 +1,115 @@
 import java.util.ArrayList;
 
 public class Main {
-    // Date: 2/25/21
-    // Aim: How can we use ArrayList's as parameters in a method?
-
-    // Do Now: Copy and paste the following in the main java file.
-
     /*
-     * 1. Write a method maxLength that takes an ArrayList of Strings as a parameter
-     * and that returns the length of the longest String in the list. If your method
-     * is passed an empty list, it should return 0.
-     */
-    public static int maxLength(ArrayList<String> list) {
-
-        String longestStr = "";
-
-        for (String string : list) {
-            if (string.length() > longestStr.length()) {
-                longestStr = string;
-            }
-        }
-
-        return longestStr.length();
-
-    }// closes method
-
-    /*
-     * 2. Write a method findSum that takes an ArrayList of Integers as a parameter
-     * and that returns the sum of all Integer objects in the list. If your method
-     * is passed an empty list, it should return 0.
-     */
-    public static int findSum(ArrayList<Integer> list) {
-
-        int sum = 0, i = 0;
-
-        while (i < list.size()) {
-
-            sum += list.get(i);
-            i++;
-
-        }
-
-        return sum;
-
-    }// closes method
-
-    /*
-     * 3a. Explain what the following mystery method does. 
-     * The following mystery method traverses the array backawards sorting elements in
-     * ascending order. An element will be moved to the front of the list if it is
-     * lesser than the element that precedes it. This will repeat until all the elements
-     * are sorted.
+     * Date: 3/2/21 Aim: How can we use Sequential/Linear searches with arrays and
+     * ArrayLists in a Java program?
      * 
-     *  public static void mystery1(ArrayList<Integer> list) { 
-     *      for(int i = list.size() - 1; i > 0; i--)
-     *      { 
-     *          if(list.get(i) < list.get(i - 1)) 
-     *          { 
-     *              int element = list.get(i);
-     *              list.remove(i);
-     *              list.add(0, element);
-     *          } 
-     *      } 
-     *      System.out.println(list); 
-     *  }  
-     * 3b.
-     * What will the mystery method return given the following lists 
-     * [2, 6, 1, 8]             ->  [1, 2, 6, 8]
-     * [30, 20, 10, 60, 50, 40] ->  [10, 20, 30, 40, 50, 60]
+     * Do Now: Read the following.
+     * 
+     * Sequential or linear search is the only method that can be used to find a
+     * value in unsorted data. It usually starts at the first element and walks
+     * through the array or list until it finds the value it is looking for and
+     * returns the index it found it at, or it loops until the end of the array or
+     * list and then it returns a -1 to show that it didn’t find the value in the
+     * array or list.
+     * 
+     * Binary searches can only be used when the data set is sorted.
      */
 
     /*
-     * 4. Write a method removeEvenLength that takes an ArrayList of Strings as a
-     * parameter and that removes all of the Strings of even length from the list.
+     * 1. Translate the method below which uses an Array of ints to an ArrayList of
+     * Integers. Explain what the method sequentialSearch does.
      */
-    public static void removeEvenLength(ArrayList<String> list) {
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).length() % 2 == 0) {
-                list.remove(i);
-                i--;
+    public static int sequentialSearch(ArrayList<Integer> elements, int target) {
+        for (int j = 0; j < elements.size(); j++) {
+            if (elements.get(j) == target) {
+                return j;
             }
         }
+        return -1;
+    }
 
-    }// closes method
+    /*
+     * 2. Write the implementation for the method where which searches for a double
+     * value in a list of Doubles. If the Double value is found the index of that
+     * value is returned otherwise -1 is returned. Due to standard error ranges with
+     * double/Double values a threshold, delta must be used to determine if the
+     * value is found and valid. To determine if a value is within the exceptable
+     * range: find the difference between the absolute value of the magicNumber and
+     * the Double at a specific index. i.e. delta = 0.01, if magicNumber is 3.45 and
+     * the value at index in realNumbers list is 3.455 then we found a Double within
+     * the accetable range since .005 is less than 0.01. 
+     */
+    public static int where(double magicNumber, ArrayList<Double> realNumbers, double delta) {
+        //implementation 
+        for (int i = 0; i < realNumbers.size(); i++) {
+            if (Math.abs(magicNumber - realNumbers.get(i)) < delta) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /*
+     * 3. Write the implemenation for the method findTitle which searches a list of
+     * Book objects and searches for a specified title. If the title is found the
+     * index of that Book object is returned. If the title is not found in any of
+     * the Book objects -1 is returned.
+     */
+    public static int findTitle(String Title, ArrayList<Book> bookList) {
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getTitle().equals(Title)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
-        ArrayList<String> stringList = new ArrayList<String>();
-        stringList.add("Hi"); // len 2
-        stringList.add("Hey"); // len 3
-        stringList.add("hello"); // len 5
-        stringList.add("hello!"); // len 6
-        stringList.add("hello!!"); // len 7
-        System.out.println(maxLength(stringList)); // should return 7
+        // Question 1
+        ArrayList<Integer> numArray = new ArrayList<Integer>();
+        numArray.add(3);
+        numArray.add(-2);
+        numArray.add(9);
+        numArray.add(38);
+        numArray.add(-23);
 
-        ArrayList<Integer> numList = new ArrayList<Integer>();
-        numList.add(100);
-        numList.add(200);
-        numList.add(300);
-        numList.add(400);
-        System.out.println(findSum(numList));
+        System.out.println("Tests of sequentialSearch");
+        System.out.println(sequentialSearch(numArray, 3));
+        System.out.println(sequentialSearch(numArray, 9));
+        System.out.println(sequentialSearch(numArray, -23));
+        System.out.println(sequentialSearch(numArray, 99));
 
-        removeEvenLength(stringList);
+        // Question 2
+        System.out.println("Tests of sequentialSearch for where method");
+        ArrayList<Double> doubleList = new ArrayList<Double>();
+        doubleList.add(2.45);
+        doubleList.add(3.6505);
+        doubleList.add(5.15);
+        doubleList.add(1.42);
+        doubleList.add(1.4105);
+        System.out.println(where(1.41, doubleList, 0.01));
+        System.out.println(where(2.45, doubleList, 0.001));
+        System.out.println(where(10.5, doubleList, 0.1));
 
-        System.out.println(stringList);
+        // Question 3
+        Book b1 = new Book("Lord of the Rings", "J.R.R. Tolkein");
+        Book b2 = new Book("I know why the caged bird sings", "Maya Angelou");
+        Book b3 = new Book("The Weary Blues", "Langston Hughes");
+        Book b4 = new Book("Basic Economics", "Thomas Sowell");
 
+        System.out.println("Tests of sequentialSearch for findTitle method");
+        ArrayList<Book> myBooks = new ArrayList<Book>();
+        myBooks.add(b1);
+        myBooks.add(b2);
+        myBooks.add(b3);
+        myBooks.add(b4);
+        System.out.println(findTitle("The Weary Blues", myBooks));
+
+        // Question 4: What is a Binary Search? Give an example of when it would be
+        // appropriate to conduct a binary search.
+        /** 
+         * A binary search involves dividing the array in half until the desired element is found, which each subsequent division getting closer to the desired element. It only works on sorted arrays, however. */
     } // closes main method
 }
