5838385a2a02733e3436f970481677450bffe42f
Matthew McCall <matthewm3004@icloud.com>
2021-03-05 17:14:48 -0500

    Lesson 64 - Insertion Sort

--- Main.java
+++ Main.java
@@ -1,88 +1,56 @@
-import java.util.ArrayList;
+import java.util.*;
 
-public class Main {
-    public static void selectionSort(int[] elements) {
-        for (int j = 0; j < elements.length - 1; j++) {
-            int minIndex = j;
-            /**
-             * A nested for loop is for a for loop that executes each iteration of the
-             * parent for loop. This mean if the parent for loop executes x amount of times
-             * and the nested for loop executes y amount of times. The body of the nested
-             * for loop executes x * y amount of times.
-             */
-            for (int k = j + 1; k < elements.length; k++) {
-                /**
-                 * This conditional checks whether the current element is smaller than the
-                 * smallest known element. If it is smaller than the smallest known element,
-                 * then it becomes the new smallest known element by having its index assigned
-                 * to minIndex. If not, we carry on and check the next element.
-                 */
-                if (elements[k] < elements[minIndex]) {
-                    minIndex = k;
-                }
-            }
-            /**
-             * These three lines of code swaps the smallest element found with the element
-             * at j.
-             */
+class Main {
+    /*Date: 3/5/21
+    Aim: How can we examine and trace through an insertion sort algorithm using an array of integers?
+    Do Now:
+    1. Examine the following method insertionSort.
+    a. What is the purpose of the temp variable?
+    // Temporarily stores the value at index j for use in a later comparison.
+    b. The purpose of possibleIndex = j probably isn't clear until you trace through the code. Read the full algorithm then explain the meaning of this line of code.
+    c. Why does the outer for loop index variable start at 1 and not 0?
+    // Because we call elements[possibleIndex - 1]
+    d. Explain the meaning of possibleIndex > 0 && temp < elements[possibleIndex - 1]
+    // Checks whether the possible index is more than zero and whether it is less than the element that preceeds it.
+    e. Explain the meaning of elements[possibleIndex] = elements[possibleIndex - 1]; possibleIndex--;
+    // Assigns the element at the current index to the element at the preceeding index. Then move the current index back to modify the preceeding index.
+    f. Explain the meaning of elements[possibleIndex] = temp;*/
+    // Reassign the current index to the value stored in temp.
+    public static void insertionSort(int[] elements) {
+        for (int j = 1; j < elements.length; j++) {
             int temp = elements[j];
-            elements[j] = elements[minIndex];
-            elements[minIndex] = temp;
-        }
-    }
-
-    public static void selectionSort(ArrayList<Integer> elements) {
-
-        for (int i = 0; i < elements.size(); i++) {
-
-            int minIndex = i;
-
-            for (int j = i; j < elements.size(); j++) {
-
-                if (elements.get(j) < elements.get(minIndex)) {
-                    minIndex = j;
-                }
-            }
-
-            elements.add(i, elements.get(minIndex));
-            elements.remove(minIndex);
-
-        }
-    }
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp < elements[possibleIndex - 1]) {
+                elements[possibleIndex] = elements[possibleIndex - 1];
+                possibleIndex--;
+            } //closes inner loop
+            elements[possibleIndex] = temp;
+        } //closes outer loop
+    }//closes method
 
     public static void main(String[] args) {
-        int[] arr1 = { 3, 86, -20, 14, 40 };
-        // System.out.println(Arrays.toString(arr1));
-        selectionSort(arr1);
-        // System.out.println(Arrays.toString(arr1));
 
-        /*
-         * 1. Examine the conditional statment within the nested (inner) loop. Explain
-         * what the conditional statement accomplishes in this part of the program
-         * 
-         * 2. After iterating through the outer loop once... temp = 3; elements[0] = -20
-         * elements[minIndex] which is elements[2] = 3. Explain the meaning of these 3
-         * lines of code. Why do they appear after the inner loop?
-         * 
-         * 
-         * 3. What will the array arr1 hold after we evaluate selectionSort(int[ ]
-         * elements) for j = 0, k = 1, k = 2, k = 3, k = 4 (1 iteration of the outer
-         * loop and 4 iterations of the inner loop - the total number of iterations
-         * needed to complete the inner loop)?
-         * 
-         * 
-         * 4. What will the array arr1 hold after we evaluate selectionSort(int[ ]
-         * elements) for j = 1, k = 1, k = 2, k = 3, k = 4 (2nd iteration of the outer
-         * loop and 4 iterations of the inner loop - the total number of iterations
-         * needed to complete the inner loop)? { -20, 3, 86, 14, 40 }
-         * 
-         * 
-         * 5. In general, what can we say about a selection sort algorithm?
-         * 
-         * 
-         * 
-         * 
-         * 6. HW - Rewrite the program using lists instead of arrays.
-         */
-    }
-}
+        /*2. What do you expect the array will print after the insertion sort is complete? Explain*/
+
+        int[] arr2 = { 3, 86, -20, 14, 40 };
+        System.out.println(Arrays.toString(arr2));
+        insertionSort(arr2);
+        System.out.println("_____________After insertion sort______________");
+        System.out.println(Arrays.toString(arr2));
+
+        /*3. Edit the for loop header by changing j < elements.length to j < 2,
+        *Make a precition about what the array values will be.
+        [3, 86, -20, 14, 40]
+        
+        4. Change the for loop condition to j < 3, make a predicition about what the array values will be.
+        [-20, 3, 86, 14, 40]
+        
+        5. Change the for loop condition to j < 4, make a predcition about what the array values will be.
+        [-20, 3, 14, 86, 40]
+        */
+
+        //6. Explain how an insertion sort works.
+        // Insertion sort works by iterating through the entire array moving smaller elements backwars as neccessary to sort the array.
+
+    }//closes main method
+}//closes class
